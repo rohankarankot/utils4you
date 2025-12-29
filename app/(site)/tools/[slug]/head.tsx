e5804a0496ba85850e685a88b1824691b86b4d60
Rohan karankot <rohankarankot@gmail.com>
2025-12-29 16:56:20 +0530

    feat: implement FAQ data seeding and update JSON-LD schema while removing deprecated SEO components

--- app/(site)/tools/[slug]/head.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Head({ params }: { params: { slug: string } }){
-  return (
-    <>
-      {/* Title & meta will be set by Seo component on the page */}
-    </>
-  );
-}
